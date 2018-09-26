50aa5846d00a99d22d7acd91952e0b110f64eebf
Lucy Copp <lucy.copp@bbc.co.uk>
2018-09-26 15:46:43 +0100

    Information added about JavaScript promises

--- JavaScriptNotes/Promises/scripts/promises.js
+++ JavaScriptNotes/Promises/scripts/promises.js
@@ -1,4 +1,4 @@
-//promise object
+///////////////////////////////////////PROMISES////////////////////////////////////////////
 
 //has states that are initially pending but change to either fulfilled or rejected
 let promise = new Promise(function(resolve, reject) { //resolve and reject are predefined   
@@ -20,5 +20,31 @@ let resolvedPromise = new Promise (function (resolve, reject){
 
 //rejected promise
 let rejectedPromise = new Promise (function (resolve, reject){
-    setTimeout (() => reject(new Error("Whoops!")),1000);
+    setTimeout (() => reject(new Error("Whoops!")), 1000);
 })
+
+
+//THEN AND CATCH
+
+promise.then(
+    function(result) { 
+        //handles a successful result 
+    },
+    function(error) {
+        //handes an error
+    }
+)
+
+//if only interested in when the state is resolved
+promise.then(alert);
+
+//if only interested in when the state is rejected
+promise.catch(error); /*OR*/ promise.then(null, error);
+
+
+//-----------------EXTRA FACTS--------------------------------
+
+//an executor can only call either resolve or reject. all further calls are ignored.
+//it is good practise to reject with error objects
+//state and result are internal so cannot directly access them 
+
