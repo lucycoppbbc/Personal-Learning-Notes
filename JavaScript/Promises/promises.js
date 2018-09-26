f40d1467f9ec2f5cebc31ffeac8b1b332034d6c3
Lucy Copp <lucy.copp@bbc.co.uk>
2018-09-26 15:14:23 +0100

    Added JavaScript folder and Promises file

---
+++ JavaScript/Promises/promises.js
@@ -0,0 +1,14 @@
+//promise object
+
+//has states that are initially pending but change to either fulfilled or rejected
+let promise = new Promise(function(resolve, reject) { //resolve and reject are predefined   
+    //the executor 
+    //when created this is ran automatically
+    //it should eventually produce a result but result is intiially undefined
+});
+
+//once executed another method is called
+//either resolve (value) were state is now fulfilled
+//or reject(error) where state is now rejected 
+
+
